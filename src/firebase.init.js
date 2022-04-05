2343c86f8f91adec78b92f8be48a1e7f69f97cb6
capmahin <mahinulhuque1996@gmail.com>
2022-04-06 03:47:34 +0600

    add firebase in vscode

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChcCRG82Ng2TkTDSEKe81ARkZlM9G4HiA",
+  authDomain: "simple-firebase-authenti-294f2.firebaseapp.com",
+  projectId: "simple-firebase-authenti-294f2",
+  storageBucket: "simple-firebase-authenti-294f2.appspot.com",
+  messagingSenderId: "817627111816",
+  appId: "1:817627111816:web:143e6d1ce9fd18cdf91a3d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
