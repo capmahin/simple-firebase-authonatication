cd94dbd13685861cb249fa237e475280e1f667dc
capmahin <mahinulhuque1996@gmail.com>
2022-04-06 05:32:23 +0600

    add provider and a click hadleer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from "./firebase.init";
 
 const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
-    console.log("working");
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div className="App">
