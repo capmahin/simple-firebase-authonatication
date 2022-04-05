5b6e9aeddd4c6bb080a4fd17c54b079100814d32
capmahin <mahinulhuque1996@gmail.com>
2022-04-06 05:23:43 +0600

    solve root warning

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import app from "./firebase.init";
 const auth = getAuth(app);
 
 function App() {
-  return <div className="App"></div>;
+  const handleGoogleSignIn = () => {
+    console.log("working");
+  };
+  return (
+    <div className="App">
+      <button onClick={handleGoogleSignIn}>google sign in</button>
+    </div>
+  );
 }
 
 export default App;
